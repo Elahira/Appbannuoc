a559f28a56afc5cac12ae4d6130c5ae3af5caad6
Khánh Sora <67995752+Elahira@users.noreply.github.com>
2021-11-29 16:21:23 +0700

    bỏ image

--- AppDrink/NuocUong.cs
+++ AppDrink/NuocUong.cs
@@ -24,7 +24,6 @@ namespace AppDrink
         public string TenNuoc { get; set; }
         public Nullable<double> Gia { get; set; }
         public Nullable<int> IdTheloai { get; set; }
-        public string Hinhanh { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<HoaDon> HoaDon { get; set; }
